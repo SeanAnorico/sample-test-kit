30f06cbceaa6e25d43145e17ff68ab48950c671d
SeanAnorico <anoricoseanalbert@gmail.com>
2022-03-11 20:37:14 +0800

    done activity

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,82 +1,151 @@
 import React from "react";
-import { View, Image, Text, Dimensions } from "react-native";
+import { View, Image, Text, Dimensions,TextInput, Button, ImageBackground } from "react-native";
+import Lottieview from "lottie-react-native";
 
 export default function TabOneScreen() {
+  
   return (
+    // main container
     <View
       style={{
         flex: 1,
-        backgroundColor: '#273746',
+        backgroundColor: '#000814',
         alignItems: 'center',
         justifyContent: 'center'
       }}
-    >
+      >
+      <ImageBackground
+        style={{
+          height: '100%',
+          width: '100%',
+          justifyContent: "center",
+          alignItems: 'center',
+        }}
+       source={{
+         uri: 'https://cdn.mos.cms.futurecdn.net/BfemybeKVXCf9pgX9WCxsc.jpg'
+        }} 
+        resizeMode="cover" 
+      >
+      {/* login container */}
       <View style={{
         height: Dimensions.get('screen').height * 0.5,
         width: '80%',
-        backgroundColor: 'white',
+        backgroundColor: '#003459',
         padding: 10,
-        borderRadius: 10
-      }}>
+        borderRadius: 10,
+        opacity: 0.8,
+      }}
+      >
+
+        {/* title container */}
+        <View
+          style={{
+            flex: 0,
+            backgroundColor: '#007EA7',
+            paddingVertical: 10,
+            paddingHorizontal: 10,
+            marginBottom: 10,
+            borderRadius: 10,
+            alignItems: 'center',
+          }}
+        >
+          <Text
+            style={{
+              color: '#FFFFFF',
+              fontFamily: 'poppins-bold',
+              fontSize: 24,
+            }}
+            numberOfLines={1}
+          >
+            LOG IN
+          </Text>
+        </View>
+
+        {/* Image container */}
         <View
           style={{
             flex: 1,
-            backgroundColor: 'red',
             marginBottom: 10,
             borderRadius: 10,
-            overflow: 'hidden'
+            overflow: 'hidden',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
-          <Image
+          <Lottieview 
+            style={{
+              height: '100%',
+              width: '100%',
+            }}
+            source={require('../assets/Animation/98303-little-robot-icon.json')}
+            autoPlay
+          />
+
+          {/* <Image
             style={{
               height: '100%',
               width: '100%',
               resizeMode: 'cover'
             }}
             source={require('../assets/images/1099445.png')}
-          />
+          /> */}
         </View>
-        <View
-          style={{
-            flex: 0,
-            backgroundColor: '#E9DBD8',
-            paddingVertical: 10,
-            paddingHorizontal: 10,
-            marginBottom: 10,
-            borderRadius: 10,
-          }}
-        >
-          <Text
-            style={{
-              color: '#877D7B',
-              fontFamily: 'poppins-bold',
-              fontSize: 18,
-            }}
-            numberOfLines={1}
-          >
-            My NFT Card
-          </Text>
-        </View>
+
+        {/* Inout container */}
         <View
           style={{
             flex: 0,
             minHeight: 100,
             maxHeight: 200,
-            backgroundColor: 'white',
+            backgroundColor: '#007EA7',
             padding: 5,
+            borderRadius: 10,
           }}
         >
-          <Text
+          <TextInput
             style={{
+              color: '#FFFFFF',
+              height: 40,
+              margin: 5,
+              borderRadius:10,
+              borderColor: '#00171F',
+              borderWidth: 1,
+              padding: 10,
               fontFamily: 'poppins-regular',
-              color: '#877D7B',
-              fontSize: 14
             }}
+            
+            placeholder='Username'
+          />
+          <TextInput
+            style={{
+              color: '#FFFFFF',
+              height: 40,
+              margin: 5,
+              borderRadius:10,
+              borderColor: '#00171F',
+              borderWidth: 1,
+              padding: 10,
+              fontFamily: 'poppins-regular',
+            }}
+            placeholder='Password'
+            secureTextEntry={true}
+          />
+          <View
+          style={{
+            margin: 10,
+            paddingHorizontal: 50,
+          }}
           >
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          </Text>
+            <Button 
+              title='Log In'
+              onPress={()=> alert("Should Be Logged In")}
+              color='#00171F'
+            />
+          </View>
         </View>
+
       </View>
+    </ImageBackground>  
     </View>
   );
 }
